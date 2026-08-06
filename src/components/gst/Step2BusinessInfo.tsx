ce904eb71ab8fecdd9bcf425de12f9034bcc5ad4
SaranDhanabal <stivensarab@gmail.com>
2026-08-06 13:32:36 +0530

    fix the bugs in gst history

--- src/components/gst/Step2BusinessInfo.tsx
+++ src/components/gst/Step2BusinessInfo.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@tanstack/react-query";
-import { fetchBasicInfo, submitGst, updateGstin, getGstin, addNewGstin } from "@/api/gst";
+import { fetchBasicInfo, submitGst, getGstin, addNewGstin } from "@/api/gst";
 import { toast } from "sonner";
 
 interface Step2Props {
